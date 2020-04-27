de09f9905646bf35d14177a065f621a60282527e
MohamedDenta <zwert1436@gmail.com>
2020-04-27 13:57:54 +0200

    ecommerce admin side

---
+++ ecommerce_adminside/lib/db/brand.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:uuid/uuid.dart';
+
+class BrandService{
+  Firestore _firestore = Firestore.instance;
+  String ref ='brands';
+  void createBrand(String name){
+    var id = Uuid();
+    String brandId = id.v1();
+
+    _firestore.collection('brands').document(brandId)
+    .setData({'brand': name});
+  }
+
+  Future<List<DocumentSnapshot>> getBrands(){
+    return _firestore.collection(ref).getDocuments().then((snaps){
+      return snaps.documents;   
+    });
+  }
+
+  Future<List<DocumentSnapshot>> getSuggestions(String suggestion) {
+    _firestore
+        .collection(ref)
+        .where('brand', arrayContains: suggestion)
+        .getDocuments()
+        .then((snaps) {
+      print(snaps.documents);
+      return snaps.documents;
+    });
+  }
+} 
---
+++ ecommerce_adminside/lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import 'screens/admin.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Admin(),
+    ),
+  );
+}
